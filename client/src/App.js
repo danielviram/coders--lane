404c4b7bd49f16338dff5a93e4f977b73d07adee
Sam Fullerton <Samdfullerton@gmail.com>
2020-12-07 16:50:00 -0600

    fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       {/* Header */}
       <Header />
       {/* JoinCards */}
-      <joinCards />
+      <JoinCards />
       {/* Swipe buttons */}
       <SwipeButtons />
     </div>
