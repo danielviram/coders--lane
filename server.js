2cb1c767ba47da74c6f62976a64182537307de90
Daniel Viramontes <danielviramo@gmail.com>
2020-12-07 15:14:33 -0600

    stuff

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(cors());
 require("./startup/routes")(app);
 require("./startup/db")();
 
+if (process.env.NODE_ENV === "production") {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, "client/build")));
+  
+    // Handle React routing, return all requests to React app
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listenining on port ${port} 🌎!`));
