6a35bb1a0044fb712f1c53c7d25a3049d20af7cf
Steve Kavuu <mrkavuu@gmail.com>
2020-12-07 16:54:24 -0600

    new login working

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,7 +11,9 @@ const usersController = {
     res.send(users);
   },
   registerUser: async (req, res) => {
+    console.log('HIT REGISTER ROUTE!!')
     const { error } = userValidator(req.body);
+    console.log('this is the err!!!!!', error)
     if (error) {
       const errorsArray = error.details.map((error) => error.message);
       return res.status(400).send(errorsArray);
@@ -19,27 +21,34 @@ const usersController = {
 
     let user = await User.findOne({ username: req.body.username });
     if (user) return res.status(400).send({ message: "Username is taken!" });
+    
+    console.log('user!!!! just find one!!', user)
 
-    const interest = await Interest.findById(req.body.interest);
-    if (!interest) return res.status(404).send({ message: 'Language was not found!' });
+   // const interest = await Interest.findById(req.body.interest);
+   // if (!interest) return res.status(404).send({ message: 'Language was not found!' });
 
     user = new User(
-      _.pick(req.body, ["firstName", "lastName", "username", "email", "password", "interest"])
+      //_.pick(req.body, ["firstName", "lastName", "username", "email", "password", "interest"])
+      _.pick(req.body, ["firstName", "lastName", "username", "email", "password"])
     );
 
+    console.log('just name new user!!', user)
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
 
     user = await user.save();
-
+    console.log('user we just made!!', user)
     user = await User.findById(user._id).populate('interest', '-__v').select("-__v -password");
 
+    
     const token = user.generateAuthToken();
 
     const resObj = {
       user: user,
       token: token,
     };
+
+    console.log('RES OBJ about ot respond!!', resObj)
 
     res.send(resObj);
   },
