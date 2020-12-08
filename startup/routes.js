1c44d7671f2d87b9c4c05924e17a5c7943acc741
Daniel Viramontes <danielviramo@gmail.com>
2020-12-07 19:56:56 -0600

    changes

--- startup/routes.js
+++ startup/routes.js
@@ -6,7 +6,6 @@ const interests = require("../routes/interests");
 
 const routes = (app) => {
   app.use(express.json());
-
   app.use("/api/v1/auth", auth);
   app.use("/api/v1/users", users);
   app.use("/api/v1/interests", interests);
